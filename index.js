d66f9e22f9e1b9ecf50591fc05761e09320152cf
Ehiz Labs <ehizlabs@gmail.com>
2023-10-01 13:16:39 +0100

    Fixed the links to public assets (css, js, images) in admin views, altered admin nav bar and some admin pages

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Variables declaration
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
@@ -15,16 +16,16 @@ mongoose.connect('mongodb://localhost:27017/Portfoliodb', {
   console.error('MongoDB connection error:', error);
 });
 
-
+// Views and public folder Setup 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-app.get("/", (req,res) =>{
+
+// Landing View Routes
+app.get('/', (req, res) =>{
   res.render("landing/pages/index.ejs");
 });
-
-
 
 app.get('/about', (req, res) => {
   res.render('landing/pages/about.ejs');
@@ -46,43 +47,54 @@ app.get('/service', (req, res) => {
   res.render('landing/pages/service.ejs');
 });
 
+// Admin View Routes
 
-
-
-
-app.get("/index", (req,res) =>{
+app.get('/admin', (req, res) =>{
   res.render("admin/pages/index.ejs");
 });
 
-app.get('/bio', (req, res) => {
+app.get('/admin/index', (req, res) =>{
+  res.render("admin/pages/index.ejs");
+});
+
+app.get('/admin/bio', (req, res) => {
   res.render('admin/pages/bio.ejs');
 });
 
 
-app.get('/contact-us', (req, res) => {
+app.get('/admin/contact-us', (req, res) => {
   res.render('admin/pages/contact-us.ejs');
 });
 
-app.get('/profile', (req, res) => {
+app.get('/admin/profile', (req, res) => {
   res.render('admin/pages/profile.ejs');
 });
 
-app.get('/project-add', (req, res) => {
-  res.render('admin/pages/project-add.ejs');
+app.get('/admin/add-project', (req, res) => {
+  res.render('admin/pages/add-project.ejs');
 });
 
-app.get('/project-edit', (req, res) => {
-  res.render('admin/pages/project-edit.ejs');
+app.post('/admin/submitNewProjectForm', (req, res) => {
+  res.sendStatus(200);
 });
 
-app.get('/projects', (req, res) => {
+app.post('/admin/submitEditProjectForm', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.get('/admin/edit-project', (req, res) => {
+  res.render('admin/pages/edit-project.ejs');
+});
+
+app.get('/admin/projects', (req, res) => {
   res.render('admin/pages/projects.ejs');
 });
 
-app.get('/services', (req, res) => {
+app.get('/admin/services', (req, res) => {
   res.render('admin/pages/services.ejs');
 });
 
+// Server listening port
 app.listen (port, () => {
   console.log(`Server running on port ${port}`);
 });
